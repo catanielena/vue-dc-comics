9cadc3f91430536887373bef07df9c1633111ab8
catanielena <catani.elena1@gmail.com>
2021-09-29 16:26:58 +0200

    Add: header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,123 @@
 <template>
-     <p>header</p>    
+    <header>
+        <div class="container header">
+            <div class="header__logo">
+                <img class="logo__img" src="@/assets/img/dc-logo.png" alt="dc-logo">
+            </div>
+            <nav class="header__nav">
+                <ul class="nav__list">
+                    <li class="list__item" v-for="(link, i) in nav" :key="`header__listItem${i}`"><a :class="{active: link.current}" href="#">{{link.text}}</a></li>            
+                </ul>
+            </nav>
+        </div>
+    </header>  
 </template>
 
 <script>
 export default {
-    name: "Header"
+    name: "Header",
+    data() {
+        return {
+            nav: [
+                {
+                    text: "characters",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "comics",
+                    url: "#",
+                    current: true
+                },
+                                {
+                    text: "movies",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "tv",
+                    url: "#",
+                    current: false
+                },
+                                {
+                    text: "games",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "collectibles",
+                    url: "#",
+                    current: false
+                },
+                                {
+                    text: "videos",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "fans",
+                    url: "#",
+                    current: false
+                },
+                                {
+                    text: "news",
+                    url: "#",
+                    current: false
+                },
+                {
+                    text: "shop",
+                    url: "#",
+                    current: false
+                }                
+            ]
+        }
+
+    }
 }
 </script>
 
-<style scoped>
-    @import '/assets/style/common'
+<style scoped lang="scss">
+    @import '@/assets/style/common';
+    @import '@/assets/style/variables';
+
+    header {
+        background-color: #fff;
+        position: fixed;
+        width: 100%;
+
+        .header {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            &__logo {
+                height: 4.375rem;
+
+                img{
+                    height: 100%
+                }
+            };
+            &__nav {
+                ul {
+                    display: flex;
+
+                    .list__item {
+                        a {
+                            line-height: $lineheightLink;
+                            margin: 0 1.25rem;
+                            font-weight: 700;
+                            color: $grey100;
+    
+                            &.active {
+                                border-bottom: 3px solid;
+                                color: $mainColor;
+                                border-color: $mainColor;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
 </style>
